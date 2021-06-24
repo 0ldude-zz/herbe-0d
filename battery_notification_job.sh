f965b70a01d2d7e2895a3d814ec38070595e880b
0ldude <geniodelletartarughe@protonmail.com>
2021-06-24 22:32:58 +0200

    n'th try

--- battery_notification_job.sh
+++ battery_notification_job.sh
@@ -5,19 +5,19 @@ bat='upower -i /org/freedesktop/UPower/devices/battery_BAT0| grep -E "state|to\ 
 # This gets the integer percentage of current battery capacity
 p=$(eval $bat | grep 'percentage' | awk '{print $2}' | awk -F '.' '{print $1}')
 
-battery_low='herbe "Unplug me Please," " " "I am too FULL"'
-battery_full='herbe "Charge me Please," " " "I am gonna SHUTDOWN"'
+bat_h=$(herbe "Unplug me Please," " " "I am too FULL")
+bat_l=$(herbe "Charge me Please," " " "Gonna SHUTDOWN")
 
 if upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep -E "state"=="charging"; then 
 	# if bat > 99% -> notify
 	if [ "$p" -gt 99 ] ; then
-		eval $battery_full
+		eval $bat_h
 	fi
 
 else
 	# if bat < 15% -> notify
 	if [ "$p" -le 15 ] ; then
-		eval $battery_low
+		eval $bat_l
 	fi
 
 fi
